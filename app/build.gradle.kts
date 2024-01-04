e9a72a82efc02e43d88c7777b8aee1849adb4ab3
Tyvsa <melody112julia@gmail.com>
2024-01-04 13:54:03 +0100

    Na zajęciach 04.01

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,6 +56,7 @@ dependencies {
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.compose.ui:ui")
+    implementation ("androidx.navigation:navigation-compose:2.7.1")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
@@ -66,5 +67,4 @@ dependencies {
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
     debugImplementation("androidx.compose.ui:ui-tooling")
     debugImplementation("androidx.compose.ui:ui-test-manifest")
-    implementation ("androidx.navigation:navigation-compose:2.4.0-alpha05")
 }
---
+++ app/src/main/java/com/example/project/FirstScreen.kt
@@ -0,0 +1,294 @@
+package com.example.project
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateListOf
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.ImeAction
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.project.ui.theme.ProjectTheme
+
+// Dodana klasa do przechowywania danych sejwów
+data class Sejw(val imie1: String, val imie2: String, val nazwa: String)
+
+@Composable
+@OptIn(ExperimentalMaterial3Api::class)
+fun SejwyScreen() {
+    var selectedSejw by remember { mutableStateOf<Sejw?>(null) }
+    var newImie1 by remember { mutableStateOf("") }
+    var newImie2 by remember { mutableStateOf("") }
+    var newNazwa by remember { mutableStateOf("") }
+    var isAddingNewSejw by remember { mutableStateOf(false) }
+    var isSettingsVisible by remember { mutableStateOf(false) }
+
+    val sejwyList = remember { mutableStateListOf<Sejw>() }
+
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(MaterialTheme.colorScheme.background)
+            .padding(16.dp)
+    ) {
+        // Przycisk do dodawania nowej konwersacji
+        if (!isAddingNewSejw && selectedSejw == null && !isSettingsVisible) {
+            Button(
+                onClick = {
+                    isAddingNewSejw = true
+                },
+                shape = RoundedCornerShape(8.dp),
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(bottom = 16.dp)
+            ) {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(4.dp)
+                ) {
+                    Text("Dodaj nową konwersację")
+                }
+            }
+        }
+
+        // Nowa konwersacja
+        if (isAddingNewSejw) {
+            Spacer(modifier = Modifier.height(16.dp))
+
+            OutlinedTextField(
+                value = newImie1,
+                onValueChange = { newImie1 = it },
+                label = { Text("Imię postaci 1") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = newImie2,
+                onValueChange = { newImie2 = it },
+                label = { Text("Imię postaci 2") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp)
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            OutlinedTextField(
+                value = newNazwa,
+                onValueChange = { newNazwa = it },
+                label = { Text("Nazwa zapisu") },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                keyboardOptions = KeyboardOptions.Default.copy(
+                    imeAction = ImeAction.Done,
+                    keyboardType = KeyboardType.Text
+                ),
+            )
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            Row(
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(8.dp),
+                horizontalArrangement = Arrangement.spacedBy(8.dp)
+            ) {
+                Button(
+                    onClick = {
+                        isAddingNewSejw = false
+                    },
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        Text("Anuluj")
+                    }
+                }
+
+                Button(
+                    onClick = {
+                        sejwyList.add(Sejw(newImie1, newImie2, newNazwa))
+                        newImie1 = ""
+                        newImie2 = ""
+                        newNazwa = ""
+                        isAddingNewSejw = false
+                    },
+                    shape = RoundedCornerShape(8.dp)
+                ) {
+                    Row(
+                        verticalAlignment = Alignment.CenterVertically,
+                        horizontalArrangement = Arrangement.spacedBy(4.dp)
+                    ) {
+                        Text("Dodaj konwersację")
+                    }
+                }
+            }
+        }
+
+        // "Ekran" dwa
+        if (selectedSejw != null && !isAddingNewSejw && !isSettingsVisible) {
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Button(
+                        onClick = {
+                            selectedSejw = null
+                        },
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        Text("Wróć")
+                    }
+
+                    Button(
+                        onClick = {
+                            isSettingsVisible = true
+                        },
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        Text("Ustawienia")
+                    }
+                }
+                Text("Kiedyś będzie tu więcej i mądrzej :(")
+            }
+        } else if (isSettingsVisible) {
+            //ekranik trzy
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                Row(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(16.dp),
+                    verticalAlignment = Alignment.CenterVertically,
+                    horizontalArrangement = Arrangement.spacedBy(8.dp)
+                ) {
+                    Button(
+                        onClick = {
+                            isSettingsVisible = false
+                        },
+                        shape = RoundedCornerShape(8.dp),
+                        modifier = Modifier
+                            .weight(1f)
+                    ) {
+                        Text("Wróć")
+                    }
+                }
+                Text("Kiedyś będzie tu więcej i mądrzej :(((")
+            }
+        } else {
+            //sejwy aka lisa
+            Box(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .background(MaterialTheme.colorScheme.background)
+            ) {
+                Card(
+                    modifier = Modifier
+                        .fillMaxSize()
+                        .padding(8.dp),
+                ) {
+                    LazyColumn(
+                        modifier = Modifier
+                            .fillMaxSize()
+                    ) {
+                        items(sejwyList) { sejw ->
+                            SejwItem(
+                                sejw = sejw,
+                                onClick = {
+                                    selectedSejw = sejw
+                                }
+                            )
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+
+
+
+@Composable
+fun SejwItem(sejw: Sejw, onClick: () -> Unit) {
+    Row(
+        modifier = Modifier
+            .fillMaxWidth()
+            .clickable(onClick = onClick)
+            .padding(16.dp),
+        verticalAlignment = Alignment.CenterVertically
+    ) {
+        // Lewa kolumna
+        Column {
+            Text(text = sejw.imie1)
+            Spacer(modifier = Modifier.height(8.dp))
+            Text(text = sejw.imie2)
+        }
+
+        // Prawa kolumna
+        Spacer(modifier = Modifier.width(16.dp))
+        Text(
+            text = sejw.nazwa,
+            modifier = Modifier.weight(1f)
+        )
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun SejwyListPreview() {
+    ProjectTheme {
+        SejwyScreen()
+    }
+}
