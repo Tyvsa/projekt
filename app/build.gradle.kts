5f914ddccb06ee4f7d6cc340ff6415b6f33db3ed
Tyvsa <melody112julia@gmail.com>
2024-01-17 23:35:39 +0100

    Room - bez zmian w firstscreenie (dzialajace)

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -56,9 +56,10 @@ dependencies {
     implementation("androidx.room:room-runtime:$room_version")
     kapt("androidx.room:room-compiler:$room_version")
 
-    implementation("androidx.core:core-ktx:1.12.0")
+    implementation ("androidx.room:room-ktx:2.2.1")
+    kapt ("androidx.room:room-compiler:2.2.1")
 
-    implementation("androidx.core:core-ktx:1.9.0")
+
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
     implementation("androidx.activity:activity-compose:1.8.1")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
